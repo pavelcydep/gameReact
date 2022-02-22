990e8e4b1b5fecef98b378a7fd18556995f61a84
Pavel <pavel.ps2014@yandex.ru>
2022-02-22 09:56:13 +0300

    Добавил history

--- крестиктиНолики/src/components/Board/Board.js
+++ крестиктиНолики/src/components/Board/Board.js
@@ -1,45 +1,25 @@
 import React from 'react';
 import './Board.css';
 import {Square} from '../Square/Square';
-import calculateWinner from '../utils/calculateWinner';
+
 export const Board=(props)=>  {
-  const [state,setState] = React.useState({
-    squares: Array(9).fill(null),
-    xIsNext: true
-  });
   
-  const handleClick=(i)=> {
-    const squares = state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = state.xIsNext ? 'X' : 'O';
-    setState({
-      squares: squares,
-      xIsNext: !state.xIsNext,
-    });
-  }
+ 
    
       const renderSquare=(i)=> {
         return <Square 
-        value={state.squares[i]}
-        onClick={()=>handleClick(i)}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
         />;
       }
-      const winner = calculateWinner(state.squares);
-      let status;
-      if (winner) {
-        status = 'Победитель: ' + winner;
-      } else {
-        status = 'Следующий игрок: ' + (state.xIsNext ? 'X' : 'O');
-      }
+     
 
      
 
 
       return (
         <div>
-          <div className="status">{status}</div>
+          
           <div className="board-row">
             {renderSquare(0)}
             {renderSquare(1)}
--- крестиктиНолики/src/components/Game/Game.js
+++ крестиктиНолики/src/components/Game/Game.js
@@ -3,16 +3,18 @@ import './Game.css';
 import calculateWinner from '../utils/calculateWinner';
 import {Board} from '../Board/Board.js';
 export const Game=(props)=> {
-console.log(props);
+
   const [state,setState] = React.useState(  {
     history: [{
       squares: Array(9).fill(null)
     }],
+    stepNumber: 0,
     xIsNext: true
   }
 );
 const handleClick=(i)=> {
-  const history = state.history;
+  const history = state.history.slice(0,state.stepNumber + 1);
+
   const current = history[history.length - 1];
   const squares = current.squares.slice();
   if (calculateWinner(squares) || squares[i]) {
@@ -23,21 +25,30 @@ const handleClick=(i)=> {
     history: history.concat([{
       squares: squares
     }]),
+    stepNumber: history.length,
     xIsNext: !state.xIsNext,
   });
 }
+
+const jumpTo=(step)=> {
+  setState({
+    stepNumber: step,
+    xIsNext: (step % 2) === 0
+  });
+}
 const history = state.history;
+
 const current = history[history.length - 1];
-const winner = props.calculateWinner(current.squares);
+const winner = calculateWinner(current.squares);
 
 const moves = history.map((step, move) => {
   const desc = move ?
     'Go to move #' + move :
     'Go to game start';
   return (
-    <li>
-      <button onClick={() => jumpTo(move)}>{desc}</button>
-    </li>
+    <li key={move}>
+    <button onClick={() => jumpTo(move)}>{desc}</button>
+  </li>
   );
 });
 
@@ -54,6 +65,7 @@ return (
       <Board
         squares={current.squares}
         onClick={(i) => handleClick(i)}
+       
       />
     </div>
     <div className="game-info">
